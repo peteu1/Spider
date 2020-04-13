e44cc365211ac9b7f044d2b62629e6184fb23d52
Pete Umland <peteu@vt.edu>
2020-04-12 20:06:59 -0400

    Improve game over screen and stats history tracking

--- app/src/main/java/com/example/peter/spider/Game/StatsActivity.java
+++ app/src/main/java/com/example/peter/spider/Game/StatsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,8 +26,8 @@ public class StatsActivity extends Activity implements View.OnClickListener {
     private final String TAG = "StatsActivity";
     // NOTE: Actual files will be prepended with difficulty integer
     public static final String STATS_HISTORY_FILE_NAME = "_stats_history.txt";
-    public static final String HIGH_SCORE_FILE_NAME = "_high_score.txt";
-    private int difficulty;
+    private String statsFileName;
+    private File statsFile;
     private Button menu;
     private String currentTime, bestTime;
     private int currentMoves, bestMoves;
@@ -37,9 +38,13 @@ public class StatsActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
+        // Initialize buttons
         menu = (Button) findViewById(R.id.menu);
         menu.setOnClickListener(this);
 
+        // Initialize access to Text Views in XML
+        TextView difficultyText = (TextView) findViewById(R.id.tv_difficulty);
+        TextView gamesPlayedText = (TextView) findViewById(R.id.tv_games_played);
         TextView currentTimeText = (TextView) findViewById(R.id.currentTime);
         TextView bestTimeText = (TextView) findViewById(R.id.bestTime);
         TextView rankTimeText = (TextView) findViewById(R.id.rankTime);
@@ -49,33 +54,54 @@ public class StatsActivity extends Activity implements View.OnClickListener {
 
         // Get current time and moves from Intent [from GameView.gameWon()]
         Intent i = getIntent();
-        difficulty = i.getIntExtra("difficulty", 0);
+        int difficulty = i.getIntExtra("difficulty", 0);
         currentTime = i.getStringExtra("currentTime");
         currentMoves = i.getIntExtra("currentMoves", -1);
-        Log.e(TAG, "currentTime:" + currentTime);
 
-        // Add stats to file & get/update high scores
-        addStats();
-        getHighScores();
-        // Update scores in UI
+        // Add stats to file & get high scores, rank
+        statsFileName = difficulty + STATS_HISTORY_FILE_NAME;
+        File filePath = getExternalFilesDir(null);
+        statsFile = new File(filePath, statsFileName);
+        int gamesPlayed = addStats();
+
+        // Update UI
+        String strDifficulty = getDifficultyText(difficulty);
+        difficultyText.setText(strDifficulty);
+        gamesPlayedText.setText(String.valueOf(gamesPlayed));
+        // Set current time, best time, and current time rank
+        int gold = getResources().getColor(R.color.colorHighScore);
         if (currentTime.equals(bestTime)) {
-            // TODO: Use Gold not yellow (yellow looks trashy)
-            currentTimeText.setTextColor(Color.YELLOW);
-            bestTimeText.setTextColor(Color.YELLOW);
-            rankTimeText.setTextColor(Color.YELLOW);
+            currentTimeText.setTextColor(gold);
+            bestTimeText.setTextColor(gold);
+            rankTimeText.setTextColor(gold);
         }
         currentTimeText.setText(currentTime);
         bestTimeText.setText(bestTime);
         rankTimeText.setText(String.valueOf(rankTime));
+
+        // Set current moves, best moves, and current moves rank
         if (currentMoves == bestMoves) {
-            // TODO: Use Gold not yellow (yellow looks trashy)
-            currentMovesText.setTextColor(Color.YELLOW);
-            bestMovesText.setTextColor(Color.YELLOW);
-            rankMovesText.setTextColor(Color.YELLOW);
+            currentMovesText.setTextColor(gold);
+            bestMovesText.setTextColor(gold);
+            rankMovesText.setTextColor(gold);
         }
         currentMovesText.setText(String.valueOf(currentMoves));
         bestMovesText.setText(String.valueOf(bestMoves));
         rankMovesText.setText(String.valueOf(rankMoves));
+    }
+
+    private String getDifficultyText(int difficulty) {
+        switch (difficulty) {
+            case 1:
+                return "Easy";
+            case 2:
+                return "Medium";
+            case 3:
+                return "Hard";
+            case 4:
+                return "Expert";
+        }
+        return "";
     }
 
     @Override
@@ -86,18 +112,59 @@ public class StatsActivity extends Activity implements View.OnClickListener {
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(i);
         }
+        // TODO: Button to reset all statistics
+        // statsFile.delete();
     }
 
-    private void addStats() {
-        /**
-         * Adds current time/moves to stats history file and get current rank
-         */
-        // TODO: First need to read everything, then re-write with new row
-        File filePath = getExternalFilesDir(null);
-        String statsFileName = String.valueOf(difficulty) + STATS_HISTORY_FILE_NAME;
-        File statsFile = new File(filePath, statsFileName);
+    private int addStats() {
+        // Adds current time/moves to stats history file and get current rank
+        int currentSeconds = convertTime(currentTime);
+        // Read stats file
+        ArrayList<String> data = readStatsFile();
+        // Get current rank & re-write output
+        rankTime = 1; rankMoves = 1;
+        int bestSeconds = currentSeconds;
+        bestMoves = currentMoves;
+        try {
+            FileWriter writer = new FileWriter(statsFile);
+            // Loop through lines of previous data
+            for (String row : data) {
+                Log.e(TAG, "row:" + row);
+                String[] items = row.split(",");
+                try {
+                    int thisSeconds = Integer.parseInt(items[0]);
+                    Log.e(TAG, "thisSeconds:" + String.valueOf(thisSeconds));
+                    if (thisSeconds < currentSeconds) {
+                        rankTime++;  // Increment rank
+                        bestSeconds = Math.min(thisSeconds, bestSeconds);  // Update high score
+                    }
+                    int thisMoves = Integer.parseInt(items[1]);
+                    Log.e(TAG, "thisMoves:" + String.valueOf(thisMoves));
+                    if (thisMoves < currentMoves) {
+                        rankMoves++;  // Increment rank
+                        bestMoves = Math.min(thisMoves, bestMoves);  // Update high score
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                writer.append(row + "\n");  // Add this line back to stats file
+            }
+            // Add new element to end of stats file
+            writer.append(currentSeconds + "," + currentMoves + "\n");
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        bestTime = convertTime(bestSeconds);
+        Log.e(TAG, "data size:" + data.size());
+        return data.size() + 1;
+    }
+
+    private ArrayList<String> readStatsFile() {
         // Read in current stats file
-        int length = (int) statsFileName.length();
+        int length = (int) statsFile.length();
         byte[] bytes = new byte[length];
         try {
             FileInputStream in = new FileInputStream(statsFile);
@@ -112,100 +179,23 @@ public class StatsActivity extends Activity implements View.OnClickListener {
             Log.e("login activity", "Can not read file: " + e.toString());
         }
         String rawData = new String(bytes);
-        String[] rows = rawData.split("\n");
-        ArrayList<String> data = new ArrayList<String>(Arrays.asList(rows));
-        Log.e(TAG, "rawData:" + rawData);
-        // Get current rank & re-write output
-        rankTime = 1;
-        rankMoves = 1;
-        int currentSeconds = convertTime(currentTime);
-        try {
-            FileWriter writer = new FileWriter(statsFile);
-            // Loop through lines of previous data
-            for (String row : data) {
-                String[] items = row.split(",");
-                try {
-                    if (convertTime(items[0]) < currentSeconds) {
-                        rankTime++;
-                    }
-                    if (Integer.parseInt(items[1]) < currentMoves) {
-                        rankMoves++;
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                // Add line back to stats file
-                writer.append(row + "\n");
-            }
-            // Add new element to end of stats file
-            writer.append(currentTime + "," + currentMoves + "\n");
-            writer.flush();
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // TODO: Don't need high score file? Set font gold if rank = 1?
-    }
-
-    private void getHighScores() {
-        /**
-         * Gets best time/moves from high score file
-         * Updates high score file if new high score
-         */
-        File filePath = getExternalFilesDir(null);
-        String highScoreFileName = String.valueOf(difficulty) + HIGH_SCORE_FILE_NAME;
-        File highScoreFile = new File(filePath, highScoreFileName);
-        int length = (int) highScoreFile.length();
-        Log.e(TAG, "highScoreFile length:" + length);
-        if (length > 0) {
-            // Get current high scores
-            byte[] bytes = new byte[length];
-            try {
-                FileInputStream in = new FileInputStream(highScoreFile);
-                try {
-                    in.read(bytes);
-                } finally {
-                    in.close();
-                }
-            } catch (FileNotFoundException e) {
-                Log.e("login activity", "File not found: " + e.toString());
-            } catch (IOException e) {
-                Log.e("login activity", "Can not read file: " + e.toString());
-            }
-            String rawData = new String(bytes);
-            String[] items = rawData.split(",");
-            bestTime = items[0];
-            bestMoves = Integer.parseInt(items[1]);
-            // Update high scores if broken
-            // TODO: don't update bests, so user can see previous best when beaten
-            if (convertTime(currentTime) < convertTime(bestTime)) {
-                bestTime = currentTime;
-            }
-            if (currentMoves < bestMoves) {
-                bestMoves = currentMoves;
-            }
-        } else {
-            // First time playing this difficulty -> new record
-            bestTime = currentTime;
-            bestMoves = currentMoves;
-        }
-        // Update high score file if any new high score
-        if ((currentTime.equals(bestTime)) || (currentMoves == bestMoves)) {
-            try {
-                highScoreFile.delete();
-                FileWriter highScorewriter = new FileWriter(highScoreFile);
-                String newHighScore = bestTime + "," + bestMoves;
-                highScorewriter.append(newHighScore);
-                highScorewriter.flush();
-                highScorewriter.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        Log.e(TAG, "rawData: " + rawData);
+        if (rawData.length() > 1) {
+            String[] rows = rawData.split("\n");
+            return new ArrayList<String>(Arrays.asList(rows));
+        } else {  // no data
+            return new ArrayList<String>();
         }
     }
 
     private int convertTime(String strTime) {
         String[] times = strTime.split(":");
         return (Integer.parseInt(times[0]) * 60) + Integer.parseInt(times[1]);
+    }
+
+    private String convertTime(int time) {
+        int minutes = time / 60;
+        int seconds = time % 60;
+        return minutes + ":" + ((seconds<10) ? "0" : "") + seconds;
     }
 }
